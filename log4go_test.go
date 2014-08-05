7756d3dec28332838fcb933ffc87802a06ae21eb
Tom Maiaroto <tom@shift8creative.com>
2014-08-04 23:08:12 -0500

    running go fix as suggested by someone else

--- log4go_test.go
+++ log4go_test.go
@@ -134,7 +134,7 @@ func TestFileLogWriter(t *testing.T) {
 	if contents, err := ioutil.ReadFile(testLogFile); err != nil {
 		t.Errorf("read(%q): %s", testLogFile, err)
 	} else if len(string(contents)) != 50 {
-		t.Errorf("malformed filelog: %q (%d characters)", string(contents), len(string(contents)))
+		t.Errorf("malformed filelog: %q (exptected 50, actual: %d characters)", string(contents), len(string(contents)))
 	}
 }
 
@@ -157,7 +157,7 @@ func TestXMLLogWriter(t *testing.T) {
 	if contents, err := ioutil.ReadFile(testLogFile); err != nil {
 		t.Errorf("read(%q): %s", testLogFile, err)
 	} else if len(string(contents)) != 185 {
-		t.Errorf("malformed xmllog: %q (%d characters)", string(contents), len(string(contents)))
+		t.Errorf("malformed xmllog: %q (expected: 185, actual: %d characters)", string(contents), len(string(contents)))
 	}
 }
 
